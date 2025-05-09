ad271d0b94af3a8ce89ca195dbfead20aad8a7f9
moya2025 <moyamariano.argentina@gmail.com>
2025-05-09 20:57:34 +0200

    f

--- js/materialize.js
+++ js/materialize.js
@@ -12337,15 +12337,7 @@ $jscomp.polyfill = function (e, r, p, m) {
 })(cash, M.anime);
 
 $('#modal1').openModal();
+$('#modal2').openModal();
 
-$('.modal-trigger').leanModal({
-  dismissible: true, // Modal can be dismissed by clicking outside of the modal
-  opacity: .5, // Opacity of modal background
-  in_duration: 300, // Transition in duration
-  out_duration: 200, // Transition out duration
-  ready: function() { alert('Ready'); }, // Callback for Modal open
-  complete: function() { alert('Closed'); } // Callback for Modal close
-}
-);
 
 
