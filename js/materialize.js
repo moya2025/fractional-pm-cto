187de293e2dc2c64f33c7827e3b68e63d53a9688
moya2025 <moyamariano.argentina@gmail.com>
2025-05-09 20:44:13 +0200

    Update materialize.js

--- js/materialize.js
+++ js/materialize.js
@@ -12338,4 +12338,14 @@ $jscomp.polyfill = function (e, r, p, m) {
 
 $('#modal1').openModal();
 
+$('.modal-trigger').leanModal({
+  dismissible: true, // Modal can be dismissed by clicking outside of the modal
+  opacity: .5, // Opacity of modal background
+  in_duration: 300, // Transition in duration
+  out_duration: 200, // Transition out duration
+  ready: function() { alert('Ready'); }, // Callback for Modal open
+  complete: function() { alert('Closed'); } // Callback for Modal close
+}
+);
+
 
